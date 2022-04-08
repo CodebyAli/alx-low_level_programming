875957ef5440e204a26756491b5f34efe4b58d08
CodebyAli <andhuimedineali@yahoo.fr>
2022-04-08 16:12:54 -0700

    Positive is better

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,11 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-/** 
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	int n;
