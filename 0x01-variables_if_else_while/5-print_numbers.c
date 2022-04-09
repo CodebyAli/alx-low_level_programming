7602c3ddf1f62946e21865358d6651ddfc768007
CodebyAli <andhuimedineali@yahoo.fr>
2022-04-08 20:32:50 -0700

    number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: 0
+*/
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	putchar(i);
+	putchar('\n');
+	
+return (0);
+}
