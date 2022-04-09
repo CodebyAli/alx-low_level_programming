7888144fbfe045691ecb3aebace6cac326cd9a68
CodebyAli <andhuimedineali@yahoo.fr>
2022-04-08 19:47:21 -0700

    Play alphabet with capital and lower case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,13 @@
 int main(void)
 {
 
-	char upper_case;
+	char lower_case;
 	char lower_case;
 
-	for (upper_case ='A'; upper_case <= 'Z'; upper_case++)
-		putchar(upper_case);
-	for (lower_case ='a'; lower_case <= 'z'; lower_case++)
+	for (lower_case = 'a'; upper_case <= 'z'; lower_case++)
 		putchar(lower_case);
+	for (lower_case = 'A'; lower_case <= 'Z'; lower_case++)
+		putchar(upper_case);
 	putchar('\n');
 
 	return (0);
