3733c085769716e8a5f76d6e93ab2214d76836a5
CodebyAli <andhuimedineali@yahoo.fr>
2022-04-08 21:15:42 -0700

    Patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: 0
+*/
+int main(void)
+{
+	int x;
+
+	while (x < 10)
+	{
+	putchar(48 + x)
+	if (x != 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	x++;
+	}
+	putchar('\n');
+	return (0);
+}
