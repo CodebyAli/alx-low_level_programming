7626d894fd89ba6711d18d19d37ff125398a3af0
CodebyAli <andhuimedineali@yahoo.fr>
2022-04-08 18:05:31 -0700

    Play Alphabet Insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: 0
+*
+*/
+int main(void)
+{
+	int c;
+	
+	for (C = 0; c < 26; c++);
+	putchar('a' + c);
+
+	putchar('\n');
+
+	return (0);
