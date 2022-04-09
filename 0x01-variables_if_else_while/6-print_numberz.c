4b90a21a49b085ce8deb5b3d96aa8b89f2d51eb3
CodebyAli <andhuimedineali@yahoo.fr>
2022-04-08 20:41:32 -0700

    numberZ

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for(i  = 0; i <= 9; i++)
+	putchar('0' + i);
+	putchar('\n');
+	return (0);
+}
