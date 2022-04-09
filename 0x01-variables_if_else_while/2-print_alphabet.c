694a10b0406af03b5d025f36fb8e7dc403dae1ec
CodebyAli <andhuimedineali@yahoo.fr>
2022-04-08 19:01:48 -0700

    Play alphabet on Insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,15 +4,15 @@
 /**
 * main - Entry point
 *
-* Return: 0
-*
+*Return: 0
 */
 int main(void)
 {
 	int c;
 
-	for (c = 0; c < 26; c++);
-	putchar('a' + c);
+	for (c = 'a'; c <= 'z'; c++);
+	putchar(c);
+
 	putchar('\n');
 
 	return (0);
