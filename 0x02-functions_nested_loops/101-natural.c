2d4f8e0741d2d2d0a0494e9d34b999ca4cb43a8f
CodebyAli <andhuimedineali@yahoo.fr>
2022-04-13 19:37:04 -0700

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - list all the natural numbers bellow 2024 
+ * 
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+
+int i, sum + 0;
+
+for (i = 0; i < 1024; i++)
+{
+if ((i % 3) == 0 || (i % 5) == 0)
+	sum += i;
+}
+printf("%d\n", sum);
+return (0);
+}
